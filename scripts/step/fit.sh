621d2a0e47c89b722c09fbdc2773d4c7b9c5ef14
Johannes Melsbach <melsbach@wim.uni-koeln.de>
2024-06-25 14:46:48 +0000

    change bs and max_len

--- scripts/step/fit.sh
+++ scripts/step/fit.sh
@@ -1,5 +1,7 @@
 python model_cli.py fit \
+ --trainer.max_epochs=2 \
+ --trainer.strategy=ddp_find_unused_parameters_true \
  --data.data_dir="../../.data/benchmarkingGS_v1-0_similarityMeasure_sequence_v3-1.csv" \
- --data.batch_size=8 \
+ --data.batch_size=2 \
  --data.tokenizer="Rostlab/prot_bert_bfd" \
- --data.max_len=8
+ --data.max_len=1024
