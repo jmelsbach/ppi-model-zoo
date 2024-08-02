0f00cc24246cc828a0ab6776123271989fed89d3
Benny2000 <bgenske@smail.uni-koeln.de>
2024-08-02 20:11:22 +0200

    Test script created for Data in Google Drive

---
+++ scripts/step/model_cli_drive_data_test.py
@@ -0,0 +1,14 @@
+from lightning.pytorch.cli import LightningCLI
+from ppi_zoo.datasets.StepGoogleDriveData import StepGoogleDriveDataModule
+from ppi_zoo.models.step.model import STEP
+from lightning.pytorch.loggers import WandbLogger
+
+def cli_main() -> None:
+    cli = LightningCLI(
+        model_class=STEP,
+        datamodule_class=StepGoogleDriveDataModule,
+        save_config_kwargs={"overwrite": True}
+    )
+
+if __name__ == '__main__':
+    cli_main()
