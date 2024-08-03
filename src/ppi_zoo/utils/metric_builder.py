3e30f560a707244c56303b934689600c3db5d10c
MartinWohlan <martin.wohlan@gmail.com>
2024-08-03 16:40:54 +0000

    create new instance of metric for each dataloader

--- src/ppi_zoo/utils/metric_builder.py
+++ src/ppi_zoo/utils/metric_builder.py
@@ -6,13 +6,15 @@ from ppi_zoo.metrics.MetricEnum import METRIC_ENUM
 def build_metrics(nr_dataloaders: int) -> nn.ModuleList:
     metrics_list = []
     for key, value in METRIC_ENUM.items():
+        build_metric = value.get('build_metric')
+        log = value.get('log')
         for dataloader_idx in range(0, nr_dataloaders):
             metrics_list.append(
                 MetricModule(
                     name=key,
-                    metric=value.get('metric'),
+                    metric=build_metric(),
                     dataloader_idx=dataloader_idx,
-                    log=value.get('log')
+                    log=log
                 )
             )
     return nn.ModuleList(metrics_list)
