f05e7983728daca7d1c3a517c50b2132367741f3
MatiHase <matihase@wlcek.de>
2024-08-22 12:14:38 +0000

    Cleaned Rapppid Dataset and Module

--- src/ppi_zoo/datasets/RapppidDataset.py
+++ src/ppi_zoo/datasets/RapppidDataset.py
@@ -1,11 +1,8 @@
 from torch.utils.data import Dataset
 import pandas as pd
 import lightning.pytorch as L
-from transformers import AutoTokenizer
 import torch
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-import re
 from ppi_zoo.utils.gold_standard_utils import create_test_split
 import pickle as pkl
 import sentencepiece as sp
@@ -13,22 +10,19 @@ import numpy as np
 
 class RapppidDataset(Dataset):
     def __init__(self, data_dir: str, file_name: str, tokenizer: object, limit: int, truncate_len: int):
-        self.data_dir = data_dir
         self.data = pd.read_csv(f'{data_dir}/{file_name}')
-        self.limit = limit or len(self.data)
-        self.tokenizer = tokenizer
-        self.truncate_len = truncate_len
+        self.limit = min(limit, len(self.data))
 
     def __len__(self):
         return len(self.data)
 
     def _tokenize(self, sequence: str, use_sentence_processor: bool = True, use_padding: bool = True) -> dict:
-        tokens = sequence[:self.truncate_len]
+        tokens = sequence[:self.hparams.truncate_len]
 
         if use_sentence_processor:
-            tokens = np.array(self.tokenizer.encode(tokens, enable_sampling=True, alpha=0.1, nbest_size=-1))
+            tokens = np.array(self.hparams.tokenizer.encode(tokens, enable_sampling=True, alpha=0.1, nbest_size=-1))
         if use_padding:
-            pad_len = self.truncate_len - len(tokens)
+            pad_len = self.hparams.truncate_len - len(tokens)
             tokens = np.pad(tokens, (0, pad_len), 'constant')
 
         return tokens
@@ -48,10 +42,6 @@ class RapppidDataset(Dataset):
 
     
 class RapppidDataModule(L.LightningDataModule):
-    # TODO: hier url übergeben
-    # url aus map rausnehmen für user
-
-    # andere params
     def __init__(
         self,
         data_dir: str = '.data',
@@ -75,20 +65,11 @@ class RapppidDataModule(L.LightningDataModule):
             with_validation: controls whether to create a validation set or not
         """
         super().__init__()
-        self.data_dir = data_dir
-        self.file_name = file_name
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        print(__file__)
         self.tokenizer = sp.SentencePieceProcessor(model_file=tokenizer_file)
-        self.with_validation = with_validation
-        self.limit = limit
-        self.truncate_len = truncate_len
 
-    # TODO: download data from cloud?
     def setup(self, stage=None):
         dataset = RapppidDataset(
-            data_dir=self.data_dir, file_name=self.file_name, tokenizer=self.tokenizer, limit=self.limit, truncate_len=self.truncate_len
+            data_dir=self.hparams.data_dir, file_name=self.hparams.file_name, tokenizer=self.tokenizer, limit=self.hparams.limit, truncate_len=self.hparams.truncate_len
         )
 
         df = dataset.data
@@ -96,8 +77,8 @@ class RapppidDataModule(L.LightningDataModule):
             df['trainTest'] == 'train'
         ]
 
-        if self.with_validation:
-            with open(f'{self.data_dir}/listHubs_human_20p_v2-1.pkl', 'rb') as f:
+        if self.hparams.with_validation:
+            with open(f'{self.hparams.data_dir}/listHubs_human_20p_v2-1.pkl', 'rb') as f:
                 protein_hubs = pkl.load(f)
             train_df, val_df = create_test_split(
                 train_df, protein_hubs=protein_hubs
@@ -116,21 +97,22 @@ class RapppidDataModule(L.LightningDataModule):
             dataset.data['trainTest'] == 'test2'
         ].tolist()
 
-        self.train_dataset = torch.utils.data.Subset(dataset, train_indices[0:self.limit])
-        self.val_dataset = torch.utils.data.Subset(dataset, val_indices[0:self.limit])
-        self.test1_dataset = torch.utils.data.Subset(dataset, test1_indices[0:self.limit])
-        self.test2_dataset = torch.utils.data.Subset(dataset, test2_indices[0:self.limit])
+        self.train_dataset = torch.utils.data.Subset(dataset, train_indices[0:self.hparams.limit])
+        self.val_dataset = torch.utils.data.Subset(dataset, val_indices[0:self.hparams.limit])
+        self.test1_dataset = torch.utils.data.Subset(dataset, test1_indices[0:self.hparams.limit])
+        self.test2_dataset = torch.utils.data.Subset(dataset, test2_indices[0:self.hparams.limit])
+        print('setup ready')
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        return DataLoader(self.train_dataset, batch_size=self.hparams.batch_size, shuffle=True, num_workers=self.hparams.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+        return DataLoader(self.val_dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=self.hparams.num_workers)
 
     def test_dataloader(self):
         return [
-            DataLoader(self.test1_dataset, batch_size=self.batch_size,
-                       shuffle=False, num_workers=self.num_workers),
-            DataLoader(self.test2_dataset, batch_size=self.batch_size,
-                       shuffle=False, num_workers=self.num_workers),
+            DataLoader(self.test1_dataset, batch_size=self.hparams.batch_size,
+                       shuffle=False, num_workers=self.hparams.num_workers),
+            DataLoader(self.test2_dataset, batch_size=self.hparams.batch_size,
+                       shuffle=False, num_workers=self.hparams.num_workers),
         ]
