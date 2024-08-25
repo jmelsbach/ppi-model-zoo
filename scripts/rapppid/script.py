f2a9886d5cc3a289bfb0c6bb247b689ba819522a
MartinWohlan <martin.wohlan@gmail.com>
2024-08-25 14:14:47 +0000

    adjust params in script.py

--- scripts/rapppid/script.py
+++ scripts/rapppid/script.py
@@ -4,10 +4,10 @@ from lightning.pytorch import Trainer
 
 data_module = RapppidDataModule(data_dir = '.data', file_name = 'benchmarkingGS_v1-0_similarityMeasure_sequence_v3-1.csv', batch_size = 80, tokenizer_file = 'scripts/rapppid/spm.model', with_validation = True, truncate_len = 1500)
 # todo: check swa parameter ()Enable Stochastic Weight Averaging.) which should be true
-model = LSTMAWD(num_codes=250, embedding_size=64, steps_per_epoch=1, num_epochs=3, lstm_dropout_rate=0.3, # todo: rename num_codes? (num_codes is the vocab_size of the tokenizer)
+model = LSTMAWD(num_codes=250, embedding_size=64, steps_per_epoch=None, num_epochs=10, lstm_dropout_rate=0.3, # todo: rename num_codes? (num_codes is the vocab_size of the tokenizer); do we num_epochs?
                 classhead_dropout_rate=0.2, rnn_num_layers=2, classhead_num_layers=2, lr=0.01, 
                 weight_decay=0.0001, bi_reduce='last', class_head_name='mult', variational_dropout=False,
-                lr_scaling=False, trunc_len=10, embedding_droprate=0.3, frozen_epochs=0, optimizer_type='ranger21')
+                lr_scaling=False, trunc_len=1500, embedding_droprate=0.3, frozen_epochs=0, optimizer_type='ranger21')
 
-trainer = Trainer(max_epochs=1)
+trainer = Trainer(max_epochs=10)
 trainer.fit(model=model, datamodule=data_module)
