063a68835edd24802256a090b15ecfe6d6ec63ed
MartinWohlan <martin.wohlan@gmail.com>
2024-09-01 13:31:10 +0000

    set random seed

--- scripts/rapppid/original_rapppid_model_cli.py
+++ scripts/rapppid/original_rapppid_model_cli.py
@@ -2,8 +2,10 @@ from lightning.pytorch.cli import LightningCLI
 from ppi_zoo.datasets.OriginalRapppidDataset import OriginalRapppidDataModule
 from ppi_zoo.models.rapppid.model import LSTMAWD
 from lightning.pytorch.loggers import WandbLogger
+import lightning.pytorch as L
 
 def cli_main() -> None:
+    L.seed_everything(5353456, workers=True)
     cli = LightningCLI(
         model_class=LSTMAWD,
         datamodule_class=OriginalRapppidDataModule,
