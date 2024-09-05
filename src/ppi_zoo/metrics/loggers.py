7ecfe50bf09a2dee30979efbfede253a43af2442
diego411 <diego.enrico@directbox.com>
2024-09-05 22:13:33 +0000

    add random classifier curves to precision recall and roc plots

--- src/ppi_zoo/metrics/loggers.py
+++ src/ppi_zoo/metrics/loggers.py
@@ -1,3 +1,5 @@
+import lightning.pytorch as L
+from torch.utils.data import DataLoader
 from lightning.pytorch import LightningModule
 from torchmetrics import (
     ConfusionMatrix,
@@ -9,6 +11,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+
+def get_dataset_balance(module: LightningModule, dataloader_idx: int, stage: str) -> float:
+    datamodule: L.LightningDataModule = module.trainer.datamodule
+    dataloader: DataLoader = None
+    if stage == 'test':
+        dataloader = datamodule.test_dataloader()[dataloader_idx] if type(datamodule.test_dataloader()) else datamodule.test_dataloader()
+    elif stage == 'fit':
+        dataloader = datamodule.train_dataloader()[dataloader_idx] if type(datamodule.train_dataloader()) else datamodule.train_dataloader()
+    elif stage == 'validate':
+        dataloader = datamodule.val_dataloader()[dataloader_idx] if type(datamodule.val_dataloader()) else datamodule.val_dataloader()
+
+    pos_count: int = 0
+    neg_count: int = 0
+
+    for batch in dataloader:
+        _, _, labels = batch        
+        pos_count += (labels == 1).sum().item()
+        neg_count += (labels == 0).sum().item()
+
+    return pos_count / (neg_count + pos_count)
 
 
 def log_confusion_matrix(module: LightningModule, confusion_matrix: ConfusionMatrix, dataloader_idx: int, stage: str):
@@ -54,9 +76,14 @@ def log_precision_recall_curve(module: LightningModule, precision_recall_curve: 
     if log_dir is None:
         return
     
+    plt.style.use('seaborn-v0_8-paper')
     fig, ax = precision_recall_curve.plot(score=True)
     ax.set_ylim(0, 1)
-    
+
+    dataset_balance: float = get_dataset_balance(module, dataloader_idx, stage)
+    ax.plot([0, 1], [dataset_balance, dataset_balance], linestyle='--', color='red', label='Random Classifier')
+    ax.legend()
+
     pr_curve_filename = os.path.join(log_dir, f'{metric_util.build_metric_log_key('precision_recall_curve', dataloader_idx, stage)}.png')
     fig.savefig(pr_curve_filename)
     plt.close(fig)  # Close the figure to avoid memory issues
@@ -82,6 +109,8 @@ def log_roc_curve(module: LightningModule, roc_curve: ROC, dataloader_idx: int, 
     
     fig, ax = roc_curve.plot(score=True)
     ax.set_ylim(0, 1)
+
+    ax.plot([0, 1], [0, 1], linestyle='--', color='red', label='Random Classifier')
 
     roc_curve_filename = os.path.join(log_dir, f'{metric_util.build_metric_log_key('roc_curve', dataloader_idx, stage)}.png')
     fig.savefig(roc_curve_filename)
