c0770607fa1ff6f63b26073745d9c1b6e046294d
Johannes Melsbach <melsbach@wim.uni-koeln.de>
2024-10-28 22:22:28 +0000

    add helper.tokenize

---
+++ src/ppi_zoo/data/rapppid/helper.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sentencepiece as sp
+
+def tokenize(
+        sequence: str,
+        tokenizer_file: str,
+        use_sentence_processor: bool = True,
+        use_padding: bool = True,
+        truncate_len: int = None,
+        random_seed: int = 5353456
+        ):
+        
+        sp.set_random_generator_seed(random_seed)
+        tokenizer = sp.SentencePieceProcessor(model_file=tokenizer_file) 
+        tokens = sequence[:truncate_len]
+
+        if use_sentence_processor:
+            tokens = np.array(tokenizer.encode(tokens, enable_sampling=True, alpha=0.1, nbest_size=-1))
+        if use_padding:
+            pad_len = truncate_len - len(tokens)
+            tokens = np.pad(tokens, (0, pad_len), 'constant')
+
+        return tokens
